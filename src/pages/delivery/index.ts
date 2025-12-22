4cbf13cf02787c946a8ab0a8dc09655cb01f9b38
joshua <tayejoshua4@gmail.com>
2025-12-22 09:38:38 +0300

    feat: add index exports for admin and delivery dashboard components

---
+++ src/pages/delivery/index.ts
@@ -0,0 +1,6 @@
+export { default as DeliveryLayout } from "./DeliveryLayout";
+export { default as DeliveryDashboard } from "./DeliveryDashboard";
+export { default as DeliveryAssignments } from "./DeliveryAssignments";
+export { default as DeliveryAssignmentDetail } from "./DeliveryAssignmentDetail";
+export { default as DeliveryHistory } from "./DeliveryHistory";
+export { default as DeliveryProfile } from "./DeliveryProfile";
