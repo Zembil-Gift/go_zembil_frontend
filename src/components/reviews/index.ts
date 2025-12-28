f63edaf88eb64ea4e96a580e0ae686e35a633ddf
joshua <tayejoshua4@gmail.com>
2025-12-28 17:47:13 +0300

    feat: add index file to export review and rating components for streamlined imports

---
+++ src/components/reviews/index.ts
@@ -0,0 +1,6 @@
+export { StarRating, RatingInput } from './StarRating';
+export { ReviewCard } from './ReviewCard';
+export { ProductReviewForm, VendorReviewForm } from './ReviewForm';
+export { RatingSummaryDisplay, CompactRating } from './RatingSummary';
+export { VendorCard } from './VendorCard';
+export { ProductReviewsSection } from './ProductReviewsSection';
